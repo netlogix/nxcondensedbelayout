a93a32c22324fd7d32276c9ae4e00242057dde9d
Stephan Schuler <stephan.schuler@netlogix.de>
2018-08-01 13:09:02 +0200

    TASK: Remove compatibility constraint for TYPO3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '7.6.0-8.6.99',
+			'typo3' => '8.6.0-8.6.99',
 			'gridelements' => '7.1.0-8.2.99',
 		],
 		'conflicts' => [
