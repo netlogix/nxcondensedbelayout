1f1d350786843b5b53146cf7dc51459210530893
Stephan Schuler <stephan.schuler@netlogix.de>
2018-08-01 10:05:50 +0200

    BUGFIX: Use l10n_display=defaultAsReadonly for position columns
    For every position related column such as tx_gridelements_backend_layout,
    tx_gridelements_container, tx_gridelements_children, colPos and sorting,
    translated records now don't show form fields but only the value
    of the source record in readonly mode. This prevents translated records
    from being positioned differnetly than their translation source but
    allows the DataHandler update mechanism to update them whenever the
    source is updated.

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,33 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
+if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-foreach (array('tx_gridelements_children', 'tx_gridelements_container', 'tx_gridelements_columns', 'tx_gridelements_backend_layout', 'colPos', 'sorting') as $columnName) {
+call_user_func(function () {
 
-	if (TYPO3_MODE === 'FE') {
-		$GLOBALS['TCA']['tt_content']['columns'][$columnName]['l10n_mode'] = 'exclude';
+	global $TYPO3_CONF_VARS;
+
+	foreach ([
+				 'tx_gridelements_children',
+				 'tx_gridelements_container',
+				 'tx_gridelements_columns',
+				 'tx_gridelements_backend_layout',
+				 'colPos',
+				 'sorting'
+			 ] as $columnName) {
+
+		if (TYPO3_MODE === 'FE') {
+			$GLOBALS['TCA']['tt_content']['columns'][$columnName]['l10n_mode'] = 'exclude';
+		}
+		if (TYPO3_MODE === 'BE') {
+			$GLOBALS['TCA']['tt_content']['columns'][$columnName]['l10n_display'] = 'defaultAsReadonly';
+		}
+
 	}
-	if (TYPO3_MODE === 'BE') {
-		$GLOBALS['TCA']['tt_content']['columns'][$columnName]['displayCond'] = 'FIELD:l18n_parent:=:0';
+
+	if (TYPO3_MODE == 'BE') {
+		// Register wizard hook to manipulate gridelements default language
+		$TYPO3_CONF_VARS['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = \Netlogix\Nxcondensedbelayout\Hooks\WizardItems::class;
 	}
 
-}
-
-if (TYPO3_MODE == 'BE') {
-    // Register wizard hook to manipulate gridelements default language
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = 'EXT:nxcondensedbelayout/Classes/Hooks/WizardItems.php:Netlogix\\Nxcondensedbelayout\\Hooks\\WizardItems';
-}
+});
