17a7bc82506d5f14a760dda35d141741e1c281d5
Stephan Schuler <stephan.schuler@netlogix.de>
2019-03-04 14:32:55 +0100

    TASK: Every plugin, menu and grid is now "language all", as well as some distinct special content
    Related: NETZSCH-1934

--- Classes/Hooks/WizardItems.php
+++ Classes/Hooks/WizardItems.php
@@ -2,63 +2,64 @@
 
 namespace Netlogix\Nxcondensedbelayout\Hooks;
 
-/*
- * This file is part of the Netlogix.Nxcondensedbelayout extension.
- *
- * This package is Open Source Software. For the full copyright and license
- * information, please view the LICENSE file which was distributed with this
- * source code.
- */
-
-use TYPO3\CMS\Backend\Controller\ContentElement\NewContentElementController;
 use TYPO3\CMS\Backend\Controller\PageLayoutController;
 use TYPO3\CMS\Backend\Wizard\NewContentElementWizardHookInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class WizardItems implements NewContentElementWizardHookInterface
 {
-	/**
-	 * Modifies WizardItems array
-	 *
-	 * Every tt_content gets either created with the current pages language
-	 * selection or "all" in case of gridelements records.
-	 *
-	 * @param array $wizardItems
-	 * @param NewContentElementController $parentObject
-	 * @return void
-	 */
-	public function manipulateWizardItems(&$wizardItems, &$parentObject)
-	{
-		$language = $this->getLanguage();
+    /**
+     * Modifies WizardItems array
+     *
+     * Every tt_content gets either created with the current pages language
+     * selection or "all" in case of gridelements records.
+     *
+     * @inheritdoc
+     */
+    public function manipulateWizardItems(&$wizardItems, &$parentObject)
+    {
+        $language = $this->getLanguage();
 
-		foreach ($wizardItems as $key => $item) {
-			if (
-				strpos($key, 'gridelements') !== false ||
-				strpos($key, 'plugins') !== false ||
-				in_array($key, ['special_menu', 'common_uploads'])
-			) {
-				if ($wizardItems[$key]['tt_content_defValues']) {
-					$wizardItems[$key]['tt_content_defValues']['sys_language_uid'] = -1;
-				}
-				if ($wizardItems[$key]['params']) {
-					$wizardItems[$key]['params'] .= '&defVals[tt_content][sys_language_uid]=-1';
-				}
-			} else {
-				if ($wizardItems[$key]['tt_content_defValues']) {
-					$wizardItems[$key]['tt_content_defValues']['sys_language_uid'] = $language;
-				}
-				if ($wizardItems[$key]['params']) {
-					$wizardItems[$key]['params'] .= '&defVals[tt_content][sys_language_uid]=' . $language;
-				}
-			}
-		}
-	}
+        foreach ($wizardItems as $key => $item) {
+            switch (true) {
+                case (!strpos($key, '_')):
+                    break;
+                case $key === 'special_menu':
+                case $key === 'special_div':
+                case $key === 'special_shortcut':
+                case $key === 'common_uploads':
+                case strpos($key, 'menu_') === 0:
+                case strpos($key, 'plugins_') === 0:
+                case (strpos($key, 'gridelements') !== false):
+                    $wizardItems[$key] = $this->setLanguageForWizardItem($item, -1);
+                    break;
+                default:
+                    $wizardItems[$key] = $this->setLanguageForWizardItem($item, (int)$language);
+                    break;
+            }
+        }
+    }
 
-	protected function getLanguage()
-	{
-		/** @var PageLayoutController $ctrl */
-		$pageLayoutController = GeneralUtility::makeInstance(PageLayoutController::class);
-		$pageLayoutController->init();
-		return (int)$pageLayoutController->current_sys_language;
-	}
+    protected function getLanguage(): int
+    {
+        $pageLayoutController = $this->getPageLayoutController();
+        $pageLayoutController->init();
+        return (int)$pageLayoutController->current_sys_language;
+    }
+
+    protected function getPageLayoutController(): PageLayoutController
+    {
+        return GeneralUtility::makeInstance(PageLayoutController::class);
+    }
+
+    protected function setLanguageForWizardItem(array $wizardItem, int $language): array
+    {
+        if ($wizardItem['tt_content_defValues']) {
+            $wizardItem['tt_content_defValues']['sys_language_uid'] = $language;
+        }
+        if ($wizardItem['params']) {
+            $wizardItem['params'] .= '&defVals[tt_content][sys_language_uid]=' . $language;
+        }
+        return $wizardItem;
+    }
 }
